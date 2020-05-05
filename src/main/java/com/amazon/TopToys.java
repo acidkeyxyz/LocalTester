d705d4e3b2c8a5237cb73b59aa1e34d9ecb5c0f0
Mendoza Osorio, Luis Salvador (lm911s) <lm911s@att.com>
2020-05-04 23:27:17 -0500

    improving for in reviews

--- src/main/java/com/amazon/TopToys.java
+++ src/main/java/com/amazon/TopToys.java
@@ -8,39 +8,36 @@ import java.util.SortedMap;
 import java.util.TreeMap;
 
 public class TopToys {
-	
-	//10:41 -11:24
-	//given list of toys, get N tops ordered by number of matches and  alphabetically in case of match
+
+	// 10:41 -11:24
+	// given list of toys, get N tops ordered by number of matches and
+	// alphabetically in case of match
 	public static void main(String[] args) {
-		String [] toys = {"woody","shrek","marshall","pitufos","barbies"};
-		String [] reviews = {
-				"woody is the best toy ever ",
-				"woody is my favorite toy",
-				"marshall from paw patrol is the best",
-				"my marshall is the best, not pitufos",
-				"barbies for girls is the best ever"};
+		String[] toys = { "woody", "shrek", "marshall", "pitufos", "barbies" };
+		String[] reviews = { "woody is the best toy ever ", "woody is my favorite toy",
+				"marshall from paw patrol is the best", "my marshall is the best, not pitufos",
+				"barbies for girls is the best ever" };
 		int tops = 2;
-		List<String> listTops = getAllTopToys(toys,tops,reviews);
+		List<String> listTops = getAllTopToys(toys, tops, reviews);
 		print(listTops);
 	}
 
 	private static List<String> getAllTopToys(String[] toys, int tops, String[] reviews) {
-		SortedMap<String,Integer> map = new TreeMap<String,Integer>();
-		for(String item:toys) {
-			map.put(item,0);
+		SortedMap<String, Integer> map = new TreeMap<String, Integer>();
+		for (String item : toys) {
+			map.put(item, 0);
 		}
-		for(String review:reviews) {
-			String []sreview = review.split(" ");
-			for(String word:sreview) {
-				if(map.containsKey(word)) {
-					map.put(word,map.get(word)+1);
-				}
+		String allreviews = String.join(" ", reviews);
+		String[] sreview = allreviews.split(" ");
+		for (String word : sreview) {
+			if (map.containsKey(word)) {
+				map.put(word, map.get(word) + 1);
 			}
 		}
-		
+
 		List<Map.Entry<String, Integer>> list = new ArrayList<Map.Entry<String, Integer>>(map.entrySet());
 		Collections.sort(list, new ValueThenKeyComparator<String, Integer>());
-		List<String> results = new ArrayList<String> ();  
+		List<String> results = new ArrayList<String>();
 		for (int i = 0; i < tops; i++) {
 			results.add(list.get(i).getKey());
 		}
@@ -48,10 +45,9 @@ public class TopToys {
 	}
 
 	private static void print(List<String> listTops) {
-		for (String  item: listTops) {
-			System.out.print(item+",");
+		for (String item : listTops) {
+			System.out.print(item + ",");
 		}
 	}
 
-	
 }
